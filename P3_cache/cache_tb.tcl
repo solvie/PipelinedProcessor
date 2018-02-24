f3d2716b1d16727e48ab29348798bdc5d028db65
DESKTOP-8O9J4BH\wodnj <jaewon.lee@mail.mcgill.ca>
2018-02-23 21:26:48 -0500

    TCL file updated

--- P3_cache/cache_tb.tcl
+++ P3_cache/cache_tb.tcl
@@ -1,37 +1,29 @@
 proc AddWaves {} {
 	;#Add waves we're interested in to the Wave window
-    add wave -position end sim:/fsm_tb/clk
-    add wave -position end sim:/fsm_tb/s_reset
-    add wave -position end sim:/fsm_tb/s_input
-    add wave -position end sim:/fsm_tb/s_output
+    add wave -position end sim:/cache_tb/clock
+    add wave -position end sim:/cache_tb/reset
+    add wave -position end sim:/cache_tb/s_addr
+    add wave -position end sim:/cache_tb/s_read
+    add wave -position end sim:/cache_tb/s_write
+    add wave -position end sim:/cache_tb/s_writedata 
+    add wave -position end sim:/cache_tb/s_waitrequest 
+    add wave -position end sim:/cache_tb/m_addr 
+    add wave -position end sim:/cache_tb/m_read 
+    add wave -position end sim:/cache_tb/m_readdata 
+    add wave -position end sim:/cache_tb/m_writedata 
+    add wave -position end sim:/cache_tb/m_waitrequest 
 }
-
-;#    clock : in std_logic;
-;#    reset : in std_logic;
-;#
-;#    -- Avalon interface --
-;#    s_addr : in std_logic_vector (31 downto 0);
-;#    s_read : in std_logic;
-;#    s_readdata : out std_logic_vector (31 downto 0);
-;#    s_write : in std_logic;
-;#    s_writedata : in std_logic_vector (31 downto 0);
-;#    s_waitrequest : out std_logic; 
-
-    m_addr : out integer range 0 to ram_size-1;
-    m_read : out std_logic;
-    m_readdata : in std_logic_vector (7 downto 0);
-    m_write : out std_logic;
-    m_writedata : out std_logic_vector (7 downto 0);
-    m_waitrequest : in std_logic
 
 vlib work
 
 ;# Compile components if any
-vcom fsm.vhd
-vcom fsm_tb.vhd
+vcom cache.vhd
+vcom cache_tb.vhd
+vcom memory.vhd
+vcom memory_tb.vhd
 
 ;# Start simulation
-vsim fsm_tb
+vsim cache_tb
 
 ;# Generate a clock with 1ns period
 force -deposit clk 0 0 ns, 1 0.5 ns -repeat 1 ns
@@ -40,4 +32,4 @@ force -deposit clk 0 0 ns, 1 0.5 ns -repeat 1 ns
 AddWaves
 
 ;# Run for 50 ns
-run 50ns
+run 100ns
