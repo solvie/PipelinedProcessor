43ae40ce20eda99f94b96e78f3d1a8b83279dabd
Solvie Lee <solvie.lee@mail.mcgill.ca>
2018-03-25 17:01:39 -0400

    removing alu ctrl from control and fixing tabs

--- P4_pipelinedprocessor/control.vhd
+++ P4_pipelinedprocessor/control.vhd
@@ -7,7 +7,7 @@ entity control is
 port(
 	clock : in std_logic;
 	reset : in std_logic;
-	-- Avalon interface --
+	
     op_code : in std_logic_vector(5 downto 0);
 	funct_code : in std_logic_vector(5 downto 0);
 	
@@ -35,72 +35,55 @@ begin
 	--parsed operation control for ALU
 	-- PLA style control parse.JPG
 	if rising_edge(clock) then
-	  if(op_code = "000000")then
-      RegDst <= '1';
-      ALUOp1 <= '1';
-      RegWrite <= '1';
-    else
-      if op_code = "100011" then
-        RegWrite <= '1';
-      else
-        RegWrite <= '0';
-      end if;
-      RegDst <= '0';
-      ALUOp1 <= '0';
-    end if;
+		if(op_code = "000000")then 
+			RegDst <= '1';
+			ALUOp1 <= '1';
+			RegWrite <= '1';
+		else
+			if op_code = "100011" then 
+				RegWrite <= '1';
+			else
+				RegWrite <= '0';
+			end if;
+			RegDst <= '0';
+			ALUOp1 <= '0';
+		end if;
         
-    if(op_code = "100011")then
-      MemtoReg <= '1';
-      MemRead <= '1';
-      ALUSrc <= '1';
-    else
-      if op_code = "101011" then
-        ALUSrc <= '1';
-      else
-        ALUSrc <= '0';
-      end if;
-      MemtoReg <= '0';
-      MemRead <= '0';
-    end if;
+		if(op_code = "100011")then
+			MemtoReg <= '1';
+			MemRead <= '1';
+			ALUSrc <= '1';
+		else
+			if op_code = "101011" then
+				ALUSrc <= '1';
+			else
+				ALUSrc <= '0';
+			end if;
+			MemtoReg <= '0';
+			MemRead <= '0';
+		end if;
     
-    if(op_code = "101011")then
-      MemWrite <= '1';
-      ALUSrc <= '1';
-    else
-      if op_code = "100011" then
-        ALUSrc <= '1';
-      else
-        ALUSrc <= '0';
-      end if;
-      MemWrite <= '0';
-    end if;
+		if(op_code = "101011")then	
+			MemWrite <= '1';
+			ALUSrc <= '1';
+		else
+			if op_code = "100011" then
+			else
+				ALUSrc <= '0';
+			end if;
+			MemWrite <= '0';
+		end if;
     
-    if(op_code = "000100")then
-      Branch <= '1';
-      MemWrite <= '1';
-      ALUOp0<='1';
-    else
-      Branch <= '0';
-      MemWrite <= '0';
-      ALUOp0<='0';
-    end if;
-    
-    -- ALU operation decoding from opcode and control parsed
-    -- ALU operation truth table.JPG
- 	  if(ALUOp1 = '0' and ALUOp0 = '0') then
- 	    ALUctrl<="010";
- 	  elsif(ALUOp1 = '0' and ALUOp0 = '1') then
- 	    ALUctrl<="110";
- 	  elsif(ALUOp1 = '1' and ALUOp0 = '0') then
- 	    case funct_code is
- 	      when "100000"=>ALUctrl<="010";
- 	      when "100010"=>ALUctrl<="110";
- 	      when "100100"=>ALUctrl<="000";
- 	      when "100101"=>ALUctrl<="001";
- 	      when "101010"=>ALUctrl<="111"; 
- 	      when others=> ALUctrl <="010";   	       	        
-      end case; 
-    end if;
+		if(op_code = "000100")then 
+			Branch <= '1';
+			MemWrite <= '1';
+			ALUOp0<='1';
+		else
+			Branch <= '0';
+			MemWrite <= '0';
+			ALUOp0<='0';
+		end if;
+
 	end if;
 	
 end process;
