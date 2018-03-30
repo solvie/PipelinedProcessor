219723641eacfd26b06746453b7a002abfc7841a
Solvie Lee <solvie.lee@mail.mcgill.ca>
2018-03-30 02:03:48 -0400

    fix

--- P4_pipelinedprocessor/control.vhd
+++ P4_pipelinedprocessor/control.vhd
@@ -35,49 +35,48 @@ begin
 	--parsed operation control for ALU
 	-- PLA style control parse.JPG
 	if rising_edge(clock) then
-else
-    if(opcode = "100000" or opcode = "001000" or opcode = "100011" or opcode = "101011") then
-      ALUcalc_operationcode <="0000";
-    elsif(opcode = "100010") then
-      ALUcalc_operationcode <="0001";
-    elsif(opcode = "011000" or opcode = "011001") then
-      ALUcalc_operationcode <="0010";
+		if(opcode = "100000" or opcode = "001000" or opcode = "100011" or opcode = "101011") then
+		  ALUcalc_operationcode <="0000";
+		elsif(opcode = "100010") then
+		  ALUcalc_operationcode <="0001";
+		elsif(opcode = "011000" or opcode = "011001") then
+		  ALUcalc_operationcode <="0010";
 
-    elsif(opcode = "011010" or opcode = "011011") then
-      ALUcalc_operationcode <="0011";
+		elsif(opcode = "011010" or opcode = "011011") then
+		  ALUcalc_operationcode <="0011";
 
-    elsif(opcode = "101010" or opcode = "001010") then
-      ALUcalc_operationcode <="0100";
+		elsif(opcode = "101010" or opcode = "001010") then
+		  ALUcalc_operationcode <="0100";
 
-    elsif(opcode = "100100" or opcode = "001100") then
-      ALUcalc_operationcode <="0101";
+		elsif(opcode = "100100" or opcode = "001100") then
+		  ALUcalc_operationcode <="0101";
 
-    elsif(opcode = "100101" or opcode = "001101") then
-      ALUcalc_operationcode <="0110";
+		elsif(opcode = "100101" or opcode = "001101") then
+		  ALUcalc_operationcode <="0110";
 
-    elsif(opcode = "100111") then
-      ALUcalc_operationcode <="0111";
+		elsif(opcode = "100111") then
+		  ALUcalc_operationcode <="0111";
 
-    elsif(opcode = "100110" or opcode = "001110") then
-      ALUcalc_operationcode <="1000";
+		elsif(opcode = "100110" or opcode = "001110") then
+		  ALUcalc_operationcode <="1000";
 
-    elsif(opcode = "010000") then
-      ALUcalc_operationcode <="1001";
-    elsif(opcode = "010010") then
-      ALUcalc_operationcode <="1010";
-    elsif(opcode = "001111") then
-      ALUcalc_operationcode <="1011";
-    elsif(opcode = "000000") then
-      ALUcalc_operationcode <="1100";
-    elsif(opcode = "000010") then
-      ALUcalc_operationcode <="1101";
-    elsif(opcode = "000011") then
-      ALUcalc_operationcode <="1110";
-    elsif(opcode = "000100" or opcode = "000101") then
-      ALUcalc_operationcode <="1111";
-    else
-      ALUcalc_operationcode <="0000";
-    end if;
+		elsif(opcode = "010000") then
+		  ALUcalc_operationcode <="1001";
+		elsif(opcode = "010010") then
+		  ALUcalc_operationcode <="1010";
+		elsif(opcode = "001111") then
+		  ALUcalc_operationcode <="1011";
+		elsif(opcode = "000000") then
+		  ALUcalc_operationcode <="1100";
+		elsif(opcode = "000010") then
+		  ALUcalc_operationcode <="1101";
+		elsif(opcode = "000011") then
+		  ALUcalc_operationcode <="1110";
+		elsif(opcode = "000100" or opcode = "000101") then
+		  ALUcalc_operationcode <="1111";
+		else
+		  ALUcalc_operationcode <="0000";
+		end if;
 
 	RegDst <= not opcode(5) and not opcode(4) and not opcode(3) and not opcode(2) and not opcode(1) and not opcode(0);
 	ALUSrc <= (opcode(5) and not opcode(4) and not opcode(3) and not opcode(2) and opcode(1) and opcode(0)) or (opcode(5) and not opcode(4) and opcode(3) and not opcode(2) and opcode(1) and opcode(0));
