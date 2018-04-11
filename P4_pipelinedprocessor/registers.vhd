bb59a8a1edb97487b1b9781c43694f53a2cbfe91
Luoqing(Ryan) Wang <wangryan7720@hotmail.com>
2018-04-11 01:57:08 -0400

    add stall counter for j

--- P4_pipelinedprocessor/registers.vhd
+++ P4_pipelinedprocessor/registers.vhd
@@ -36,6 +36,7 @@ architecture arch of registers is
 type registers_body is array(0 to 31) of std_logic_vector(31 downto 0);
 signal register_block : registers_body :=(others=>"00000000000000000000000000000000");
 signal s_jumping : std_logic;
+signal stall_r : integer := 0;
 
 file file_Output : text;
 --https://en.wikibooks.org/wiki/MIPS_Assembly/Instruction_Formats
@@ -54,7 +55,7 @@ begin
     		data_out_left<="00000000000000000000000000000000";
       data_out_right<="00000000000000000000000000000000";
 		end if;
-	  if (reset = '1') then
+	if (reset = '1') then
 	    --reset all registers
 			register_block<=(others=>"00000000000000000000000000000000");
 				jumping <='0';
@@ -66,7 +67,11 @@ begin
        	r_s<="00000";
        	opcode<="000000";
        	pseudo_address <="00000000000000000000000000";
-		elsif( instruction(31 downto 27) ="00001") THEN
+
+    elsif(stall_r = 0) then
+
+
+		if( instruction(31 downto 27) ="00001") THEN
 			jumping <='1';
 			data_out_left<="00000000000000000000000000000000";
 			data_out_right<="00000000000000000000000000000000";
@@ -76,7 +81,8 @@ begin
 			r_s<="00000";
 			opcode<="000000";
 			pseudo_address<= instruction(25 downto 0);
-		  n_pseudo_address<= std_logic_vector(resize(unsigned(instruction(25 downto 0)),32));
+		    n_pseudo_address<= std_logic_vector(resize(unsigned(instruction(25 downto 0)),32));
+		    stall_r <= 3;
 		elsif ( instruction ="UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU") THEN
 			jumping <='0';
 			data_out_left<="00000000000000000000000000000000";
@@ -89,49 +95,53 @@ begin
 			pseudo_address <="00000000000000000000000000";
 		else
 			jumping <='0';
-		  opcode <=instruction(31 downto 26);
-		  if(instruction(31 downto 26)="000000") then
+		  	opcode <=instruction(31 downto 26);
+		  	if(instruction(31 downto 26)="000000") then
 				--R type
 				data_out_left<=register_block(to_integer(unsigned(instruction(25 downto 21))));
 				data_out_right<=register_block(to_integer(unsigned(instruction(20 downto 16))));
 				r_s<=instruction(15 downto 11);
-		elsif( (instruction(31 downto 26) = "101011")--sw
-		   ) then
+			elsif( (instruction(31 downto 26) = "101011")--sw
+		   	) then
 		  		data_out_left<="00000000000000000000000000000000";
 		  		data_out_right<="00000000000000000000000000000000";
 		  		data_memory_data <= register_block(to_integer(unsigned(instruction(20 downto 16))) + to_integer(unsigned(instruction(15 downto 0))));
 		  		data_memory_address <= std_logic_vector(resize(unsigned(instruction(25 downto 21)),32));
 		  		r_s<="00000";
-		  elsif( (instruction(31 downto 26) = "100011")--lw
-		   ) then
+		  	elsif( (instruction(31 downto 26) = "100011")--lw
+		   	) then
 		  		data_out_left<="00000000000000000000000000000000";
 		  		data_out_right<="00000000000000000000000000000000";
 		  		data_memory_address <= std_logic_vector(resize(unsigned(instruction(25 downto 21)),32));
 		  		r_s<=instruction(15 downto 11);
-		  else
+		  	else
 			--Itype
 		    data_out_left<=register_block(to_integer(unsigned(instruction(25 downto 21))));
 		    data_out_right<=register_block(to_integer(unsigned(instruction(15 downto 11))));
 		    r_s<=instruction(20 downto 16);
-		  end if;
+		  	end if;
 
-		  shamt<=instruction(10 downto 6);
-		  funct<=instruction(5 downto 0);
+		  	shamt<=instruction(10 downto 6);
+		  	funct<=instruction(5 downto 0);
 			pseudo_address<= instruction(25 downto 0);
-		  n_pseudo_address<= std_logic_vector(resize(unsigned(instruction(25 downto 0)),32));
-		  if(
-        (instruction(31 downto 26) = "001100")or --andi
-        instruction(31 downto 26) = "001101" --ori
-      ) then
-      --zero extension
-      data_out_imm <= std_logic_vector(resize(unsigned(instruction(15 downto 0)),32));--zero extend
-      elsif((instruction(31 downto 26) = "101011")) then 
-      data_out_imm <= "00000000000000000000000000000000";
-      elsif((instruction(31 downto 26) = "100011")) then 
-      data_out_imm <= "00000000000000000000000000000000";
-      else
-      data_out_imm <= std_logic_vector(resize(signed(instruction(15 downto 0)),32));--sign extend
-      end if;
+		  	n_pseudo_address<= std_logic_vector(resize(unsigned(instruction(25 downto 0)),32));
+		  	if(
+        		(instruction(31 downto 26) = "001100")or --andi
+        		instruction(31 downto 26) = "001101" --ori
+      		) then
+      		--zero extension
+	      data_out_imm <= std_logic_vector(resize(unsigned(instruction(15 downto 0)),32));--zero extend
+	      elsif((instruction(31 downto 26) = "101011")) then 
+	      data_out_imm <= "00000000000000000000000000000000";
+	      elsif((instruction(31 downto 26) = "100011")) then 
+	      data_out_imm <= "00000000000000000000000000000000";
+	      else
+	      data_out_imm <= std_logic_vector(resize(signed(instruction(15 downto 0)),32));--sign extend
+	      end if;
+    	end if;
+    else
+    	
+    	stall_r <= stall_r - 1;
     end if;
 end if;
 end process;
